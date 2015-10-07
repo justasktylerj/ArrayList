d01d5cd5480731e8aaca6d05213996102583c4e5
Tyler jarrard <tjar2074@740s-pl-104.local>
2015-10-07 09:54:54 -0600

    does things

--- src/list/controller/ArrayListController.java
+++ src/list/controller/ArrayListController.java
@@ -1,10 +1,28 @@
 package list.controller;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 public class ArrayListController 
 {
+	private ArrayList<String> firstWords;
+	
+	public ArrayListController()
+	{
+		firstWords = new ArrayList<String>();
+	}
 	
 	public void start()
 	{
-		
+		firstWords.add("DOOOWEEEEEOOOOOOOH");
+		firstWords.add("dunnadunnadunnadunnadunnadunnadunnaDUNDUN");
+	}
+	
+	private void showList()
+	{
+		for(int spot = 0; spot < firstWords.size(); spot++)
+		{
+			JOptionPane.showMessageDialog(null, firstWords.get(spot) + " is a spot# " + spot);
+		}
 	}
 }
